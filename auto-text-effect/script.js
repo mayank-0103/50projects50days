64c2616a8062688040f29a7c9c1baaff482332f6
mayank-0103 <201279422+mayank-0103@users.noreply.github.com>
2025-03-12 21:49:46 +0530

    updated code

--- auto-text-effect/script.js
+++ auto-text-effect/script.js
@@ -1,22 +1,21 @@
-const textEl = document.getElementById('text')
-const speedEl = document.getElementById('speed')
-const text = 'We Love Programming!'
-let idx = 1
-let speed = 300 / speedEl.value
+const textEl = document.getElementById('text');
+const speedEl = document.getElementById('speed');
+const text = 'xAI is an AI company with the mission of advancing scientific discovery and gaining a deeper understanding of our universe.';
+let idx = 1;
+let speed = 300 / speedEl.value;
 
-writeText()
+writeText();
 
 function writeText() {
-    textEl.innerText = text.slice(0, idx)
+    textEl.innerText = text.slice(0, idx);
 
-    idx++
+    idx++;
 
     if(idx > text.length) {
-        idx = 1
+        idx = 1;
     }
 
-    setTimeout(writeText, speed)
+    setTimeout(writeText, speed);
 }
 
-
-speedEl.addEventListener('input', (e) => speed = 300 / e.target.value)
+speedEl.addEventListener('input', (e) => speed = 300 / e.target.value);
