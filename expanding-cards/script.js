66fa8ad6d140e9512a9f5de456b55ff78ffae16b
mayank-0103 <201279422+mayank-0103@users.noreply.github.com>
2025-03-12 12:07:08 +0530

    modified by Mayank

--- expanding-cards/script.js
+++ expanding-cards/script.js
@@ -2,13 +2,13 @@ const panels = document.querySelectorAll('.panel')
 
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
+        removeActiveClasses();
+        panel.classList.add('active');
     })
 })
 
 function removeActiveClasses() {
     panels.forEach(panel => {
-        panel.classList.remove('active')
+        panel.classList.remove('active');
     })
 }
